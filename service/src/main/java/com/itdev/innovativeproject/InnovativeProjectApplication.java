2a236d0f11d0a10705a08300c30cf353f1e9b6d2
IiLuha <elyamin@bk.ru>
2023-09-23 12:11:07 +0300

    Initial commit

---
+++ service/src/main/java/com/itdev/innovativeproject/InnovativeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.itdev.innovativeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovativeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnovativeProjectApplication.class, args);
+    }
+
+}
---
+++ service/src/test/java/com/itdev/innovativeproject/InnovativeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itdev.innovativeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovativeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
